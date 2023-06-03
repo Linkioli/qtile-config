9820c163fba0fcc15f109dfb70d6d89b0e3719a2
Linkioli <malachishaffer@protonmail.com>
2023-06-03 14:35:15 -0400

    removed keybindings I don't need

--- config.py
+++ config.py
@@ -41,24 +41,7 @@ keys = [
     Key([mod], "n", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "e", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "i", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "n", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "e", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     Key([mod], "m", lazy.layout.swap_main(), desc="Promote focused window to master"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key(
-        [mod, "shift"],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
